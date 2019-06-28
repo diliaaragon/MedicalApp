ead849f736f4ae0c4f8c4904ddc41f6e07e4e4e4
diliaaragon <dilia.aragon@koombea.com>
2019-06-28 11:58:42 -0500

    Create doctor model  with its validations.

---
+++ app/models/doctor.rb
@@ -0,0 +1,17 @@
+class Doctor < ApplicationRecord
+  validates :first_name, presence: true, length: { minimum: 3, maximum: 20 }
+  validates :last_name, presence: true, length: { minimum: 7, maximum: 30 }
+  validates :identification, presence: true, uniqueness: { case_sensitive: false },
+                             numericality: true, length: { in: 8..12 }
+  validates :professional_card, presence: true, uniqueness: { case_sensitive: false },
+                                numericality: true, length: { in: 8..20 }
+  validates :birthdate, presence: true
+  validates :speciality, presence: true
+  validate :birhdate_cannot_be_in_the_future
+
+  def birhdate_cannot_be_in_the_future
+    if birthdate.present? && birthdate > Date.today
+      errors.add(:birthdate, "can't be in the future.")
+    end
+  end
+end
